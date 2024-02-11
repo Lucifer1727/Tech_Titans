fde624e1ab455342f90d55c8e37548ee41327e41
Lucifer1727 <jayeshraiger2708@gmail.com>
2024-02-11 20:23:52 +0530

    Adding more functionality

--- src/App.js
+++ src/App.js
@@ -1,55 +1,85 @@
-
+import React, { useState } from "react";
 import main from "./Images/main-heading.png";
-import bg from "./Images/bg.jpg"; 
+import bg from "./Images/bg.jpg";
 import logo from "./Images/logo.png";
-import why from "./Images/why-wonder-games-heading-desktop.png"
-import hair from "./Images/Special/svg4.svg"
-import star from "./Images/Special/svg5.svg"
-import whore from "./Images/Special/svg3.svg"
-import clinical from "./Images/Special/clinically-proven-excellence.png"
-import comm from "./Images/Special/community.png"
-import access from "./Images/Special/accessible-skill-development.png"
-import './App.css';
+import why from "./Images/why-wonder-games-heading-desktop.png";
+import hair from "./Images/Special/svg4.svg";
+import star from "./Images/Special/svg5.svg";
+import whore from "./Images/Special/svg3.svg";
+import clinical from "./Images/Special/clinically-proven-excellence.png";
+import comm from "./Images/Special/community.png";
+import access from "./Images/Special/accessible-skill-development.png";
+import icon from "./Images/Icons.png";
+import "./App.css";
 
-
-
-
-
-
-
-
-
-
-
-
-function NavBar() { 
+function NavBar() {
+  const [openDropdown, setOpenDropdown] = useState(null);
+  const handleButtonClick = (dropdownId) => {
+    setOpenDropdown(openDropdown === dropdownId ? null : dropdownId);
+  };
   return (
     <>
       <>
-        <div class="flex justify-between bg-[#F8CB1E] border-b-[1px] h-[9vh] border-[#211D21]">
-        <div classname="flex mt-3 min-ml-[10px]">
-          <img class="mt-1 ml-[50px]" src={logo} alt="" />
+        <div class="flex justify-between bg-[#F9f9f9] border-b-[1px] h-[9vh] ">
+          <div classname="flex mt-3 min-ml-[10px]">
+            <img class="mt-1 ml-[50px]" src={logo} alt="" />
           </div>
           <div class="flex m-2">
-            <div class=" hover:text-[f9f9f9] flex font-bold text-[#211D21] font-['Josefin Sans', sans-serif] text-[1.7em] m-4 mt-7 subpixel-antialiased">
-              <a className="h-fit" href="">About us</a>
+            <div class="font-bold text-black drop text-xl m-4 mt-7">
+              <a className="h-fit" href="">
+                About Us
+              </a>
             </div>
-            <div class=" hover:text-[f9f9f9] font-bold flex text-[#211D21]  text-[1.7em] m-4 mt-7  subpixel-antialiased">
-             <a className="h-fit" href=""> Skills we develop</a>
+            <div className="group inline-block drop text-center">
+              <button
+                onClick={() => handleButtonClick("dropdown1")}
+                className="font-bold text-black text-xl m-4 mt-7"
+              >
+                Skills we Develop
+              </button>
+              {openDropdown === "dropdown1" && (
+                <div className="absolute mt-2 w-48 text-black bg-white border border-black">
+                  <a href="#" className="block px-4 py-2 font hover:bg-gray-200">
+                    Cognitive SKills
+                  </a>
+                  <a href="#" className="block px-4 py-2 font hover:bg-gray-200">
+                    Educational Skills
+                  </a>
+                </div>
+              )}
             </div>
-            <div class=" <a>hover:text-[f9f9f9] font-bold flex text-[#211D21]  text-[1.7em] m-4 mt-7  subpixel-antialiased">
-              <a className="h-fit" href="">Who we help</a>
+            <div className="group inline-block drop text-center">
+              <button
+                onClick={() => handleButtonClick("dropdown2")}
+                className="font-bold text-black text-xl m-4 mt-7"
+              >
+                Who we help
+              </button>
+              {openDropdown === "dropdown2" && (
+                <div className="absolute mt-2 w-48 text-black bg-white border border-black">
+                  <a href="#" className="block font px-4 py-2 hover: bg-gray-200">
+                    Attention Deficit Hyperactive Disorder
+                  </a>
+                  <a href="#" className="block px-4 py-2 font hover:bg-gray-200">
+                    Autism Spectrum Disorder
+                  </a>
+                  <a href="#" className="block px-4 py-2 font hover:bg-gray-200">
+                    Dyslexia
+                  </a>
+                </div>
+              )}
             </div>
-            <div class=" hover:text-[f9f9f9] font-bold flex text-[#211D21] text-[1.7em] m-4 mt-7  subpixel-antialiased">
-              <a className="h-fit" href="">Clinical Advice</a> 
+            <div class="font-bold drop text-black text-xl m-4 mt-7">
+              <a className="h-fit" href="">
+                Clinical Advice
+              </a>
             </div>
-            <div class=" hover:text-[f9f9f9] font-bold flex text-[#211D21] text-[1.7em] m-4 mt-7  subpixel-antialiased">
-              <a className="h-fit" href="">About Us</a>
+            <div class="font-bold drop text-black text-xl m-4 mt-7">
+              <a className="h-fit" href="">
+                Log In
+              </a>
             </div>
-            <div class=" hover:text-[f9f9f9] font-bold flex text-[#211D21] text-[1.7em] m-4 mt-7 mr-12 subpixel-antialiased">
-              <a className="h-fit" href="">Log In</a>
-            </div>
-          </div> 
+          </div>
         </div>
       </>
     </>
@@ -57,108 +87,156 @@ function NavBar() {
 }
 
 function MainPart() {
+
+  const image = [
+    "./Images/Start/discover-wondertree.jpg",
+    "./Images/Start/educational-games.jpg",
+    "./Images/Start/spectrum-games.jpg",
+    "./Images/Start/for-institution-vertical.jpg"
+  ];
+
   return (
     <>
-      <div style={{
-        backgroundImage: `url(${bg})`,
-      }} class="h-screen flex flex-col justify-center ">
-        
-        
-        <div class="bg-transparent pb-[35px] mt-[60vh]  relative flex justify-center">
-          <div class="w-fit h-fit bg-transparent flex justify-around px-[35px]">
-            <div class="text-center w-[104.93] px-[20px] py-[10px] leading-6 shadow bg-blend-multiply shadow-black text- rounded-lg font-bold bg-[#F8CB1E] min-h-full">
-              Cognitive Learning
-            </div>
-            <div class="text-center w-[104.93] mx-[15px]  px-[20px] py-[10px] shadow bg-blend-multiply shadow-black leading-6 font-bold text-[#211D21] rounded-lg bg-[#F8CB1E] h-fit">
-              Educational Learning
-            </div>
-            <div class="text-center w-[104.93] mx-[15px]  px-[20px] py-[10px] shadow bg-blend-multiply shadow-black leading-6 font-bold text-[#211D21] rounded-lg bg-[#F8CB1E] h-fit">
-              Educational Learning
-            </div>
-            <div class="text-center w-[104.93] mx-[15px]  px-[20px] py-[10px] shadow bg-blend-multiply shadow-black leading-6 font-bold text-[#211D21] rounded-lg bg-[#F8CB1E] h-fit">
-              Educational Learning
-            </div>
-            <div class="text-center w-[104.93] mx-[15px]  px-[20px] py-[10px] shadow bg-blend-multiply shadow-black leading-6 font-bold text-[#211D21] rounded-lg bg-[#F8CB1E] h-fit">
-              Educational Learning
-            </div>
-            <div class="text-center w-[104.93] mx-[15px]  px-[20px] py-[10px] shadow bg-blend-multiply shadow-black leading-6 font-bold text-[#211D21] rounded-lg bg-[#F8CB1E] h-fit">
-              Educational Learning
-            </div>
-            <div class="text-center w-[104.93] px-[20px] py-[10px] leading-6 shadow bg-blend-multiply shadow-black font-bold text-[#211D21] rounded-lg bg-[#F8CB1E] h-fit">
-              Parental Teach
-            </div>
-          </div>
-        </div>
-        <div class="flex justify-center bg-transparent">
+      <div
+        style={{
+          backgroundImage: `url(${bg})`,
+        }}
+        class="h-screen flex flex-col justify-center "
+      >
+        <div class="flex justify-center mt-[60vh] bg-transparent">
           <div class="w-[130vh] flex justify-around rounded-[25px] shadow bg-blend-multiply shadow-black h-[70vh] bg-[#F8CB1E]">
+            <div className="flex flex-col">
+            </div>
           </div>
         </div>
-        <div className="flex">
-        <img class="m-3" src={main} className="justify-center" alt="" />
+        <div className="flex items-center">
+          <img
+            class="m-3"
+            src={main}
+            className="justify-center items-center"
+            alt=""
+          />
         </div>
       </div>
-      <div style={{
-        backgroundImage: `url(${bg})`,
-      }} class="h-screen flex flex-col justify-center "></div>
+      <div
+        style={{
+          backgroundImage: `url(${bg})`,
+        }}
+        class="h-screen flex flex-col justify-center "
+      ></div>
     </>
   );
 }
-function WhyGames()
-{
-  return(
+function WhyGames() {
+  return (
     <>
-    <div className="flex flex-col m-[30vh] mt-0 w-fit h-fit bg-blend-multiply bg-[#F9f9f9]">
-      <div className="flex justify-center items-center">
-        <img src={why} alt=""/>
+      <div
+        id="why"
+        className="flex flex-col m-[30vh] mt-0 w-fit h-fit bg-blend-multiply bg-[#F9f9f9]"
+      >
+        <div className="flex justify-center items-center">
+          <img src={why} alt="" />
+        </div>
+        <div className="flex m-[10px]">
+          <div className="flex flex-col p-[30px]">
+            <img src={hair} className="w-[20vh] h-[20vh]" alt="" />
+            <img src={star} className="w-[10vh] h-[10vh]" alt="" />
+            <img src={whore} className="w-[10vh] h-[10vh]" alt="" />
+          </div>
+          <div className="flex flex-col p-[30px] items-center justify-center ">
+            <img src={clinical} className="" alt="" />
+            <h1 className="  clinical">
+              Clinically Proven
+              <br /> Excellence
+            </h1>
+            <div>
+              <p className="clinical2">Select NeuroNurturer for our </p>
+              <p className="clinical2">clinically validated AR games that offer,</p>
+              <p className="clinical2">genuine advantages including</p>
+              <p className="clinical2">improving balance, bolstering self-</p>
+              <p className="clinical2">esteem, and providing practical</p>
+              <p className="clinical2">guidance. This results in significant</p>
+              <p className="clinical2">improvements for children with</p>
+              <p className="clinical2">learning differences.</p>
+            </div>
+          </div>
+          <div className="p-[30px] ">
+            <img src={access} className="" alt="" />
+            <h1 className="  clinical">
+              Accessible Skill
+              <br />
+              Development
+            </h1>
+            <div>
+              <p className="clinical2">Every child deserves the opportunity</p>
+              <p className="clinical2">to unlock their potential. WonderTree</p>
+              <p className="clinical2">is dedicated to fostering accessible</p>
+              <p className="clinical2">skill development across all</p>
+              <p className="clinical2">spectrums, making learning</p>
+              <p className="clinical2">enjoyable and inclusive, regardless</p>
+              <p className="clinical2">
+                of individual abilities or challenges.
+              </p>
+              <p className="clinical2">learning differences.</p>
+            </div>
+          </div>
+          <div className="p-[30px] ">
+            <img src={comm} className="" alt="" />
+            <h1 className="  clinical">
+              By the community,
+              <br />
+              For the community
+            </h1>
+            <div>
+              <p className="clinical2">We excel by being community-</p>
+              <p className="clinical2">driven, collaboratively developing</p>
+              <p className="clinical2">solutions with educators, therapists,</p>
+              <p className="clinical2">and parents to ensure that every</p>
+              <p className="clinical2">aspect of our platform meets the</p>
+              <p className="clinical2">diverse needs of children.</p>
+            </div>
+          </div>
+        </div>
+        <img src={icon}></img>
       </div>
-      <div className="flex m-[10px]">
-        <div className="flex flex-col p-[30px]">
-          <img src={hair} className="w-[20vh] h-[20vh]" alt=""/>
-          <img src={star} className="w-[10vh] h-[10vh]" alt=""/>
-          <img src={whore} className="w-[10vh] h-[10vh]" alt=""/>
+    </>
+  );
+}
+
+function CognitiveSkills() {
+  return (
+    <>
+    <h1 className="clinical ml-[75vh]">---------------------------------------<h1 className="cognitive">Cognitive Skils</h1>---------------------------------------</h1>
+      <div class="flex justify-center bg-transparent">
+          <div class="w-[160vh] flex justify-around rounded-[25px] shadow bg-blend-multiply shadow-black h-[70vh] bg-[#F8CB1E]">
+            <div className="bg-[f9f9f9] flex">
+              <div className="flex flex-col">
+                
+              </div>
+            </div>
+            <div className="bg-[f9f9f9]"></div>
+            <div className="bg-[f9f9f9]"></div>
+          </div>
         </div>
-        <div className="flex flex-col p-[30px] items-center justify-center ">
-        <img src={clinical} className="" alt=""/>
-        <h1 className="  clinical">Clinically Proven<br/> Excellence</h1>
-        <div>
-           <p className="clinical2">Select WonderTree for our clinically</p>
-           <p className="clinical2">validated AR games that offer</p>
-           <p className="clinical2">genuine advantages, including</p>
-           <p className="clinical2">improving balance, bolstering self-</p>
-           <p className="clinical2">esteem, and providing practical</p>
-           <p className="clinical2">guidance. This results in significant</p>
-           <p className="clinical2">improvements for children with</p>
-           <p className="clinical2">learning differences.</p>
+    </>
+  );
+}
+
+function EducationaLSkills() {
+  return (
+    <>
+    <h1 className="clinical mt-[30vh] ml-[75vh]">---------------------------------------<h1 className="cognitive">Educational Skils</h1>---------------------------------------</h1>
+      <div class="flex justify-center bg-transparent">
+          <div class="w-[160vh] flex justify-around rounded-[25px] shadow bg-blend-multiply shadow-black h-[70vh] bg-[#F8CB1E]">
+            <div className="bg-[f9f9f9] flex">
+              <div className="flex flex-col">
+                
+              </div>
+            </div>
+            <div className="bg-[f9f9f9]"></div>
+            <div className="bg-[f9f9f9]"></div>
+          </div>
         </div>
-        </div>
-        <div className="p-[30px] ">
-        <img src={access} className="" alt=""/>
-        <h1 className="  clinical">Accessible Skill<br/>Development</h1>
-        <div>
-           <p className="clinical2">Every child deserves the opportunity</p>
-           <p className="clinical2">to unlock their potential. WonderTree</p>
-           <p className="clinical2">is dedicated to fostering accessible</p>
-           <p className="clinical2">skill development across all</p>
-           <p className="clinical2">spectrums, making learning</p>
-           <p className="clinical2">enjoyable and inclusive, regardless</p>
-           <p className="clinical2">of individual abilities or challenges.</p>
-           <p className="clinical2">learning differences.</p>
-        </div>
-        </div>
-        <div className="p-[30px] ">
-        <img src={comm} className="" alt=""/>
-        <h1 className="  clinical">By the community,<br/>For the community</h1>
-        <div>
-           <p className="clinical2">We excel by being community-</p>
-           <p className="clinical2">driven, collaboratively developing</p>
-           <p className="clinical2">solutions with educators, therapists,</p>
-           <p className="clinical2">and parents to ensure that every</p>
-           <p className="clinical2">aspect of our platform meets the</p>
-           <p className="clinical2">diverse needs of children.</p>
-        </div>
-        </div>
-      </div>
-    </div>
     </>
   );
 }
@@ -169,6 +247,8 @@ export default function App() {
       <NavBar></NavBar>
       <MainPart></MainPart>
       <WhyGames></WhyGames>
+      <CognitiveSkills></CognitiveSkills>
+      <EducationaLSkills></EducationaLSkills>
     </>
   );
 }
